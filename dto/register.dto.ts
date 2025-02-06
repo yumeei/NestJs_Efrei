7db4bb72c3815a82906f82f450abd96c365cbbdb
Valérie Song <valerie.song@ormex.io>
2025-02-06 16:16:01 +0100

    aout dto

---
+++ dto/register.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsEmail, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
