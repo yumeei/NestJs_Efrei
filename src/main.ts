f95aba7cfd5dc4b1bcfacf4f0a23557d837a0186
Valérie Song <valerie.song@ormex.io>
2025-02-06 16:20:18 +0100

    main.ts

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+console.log('JWT_SECRET from main.ts:', process.env.JWT_SECRET);
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API NestJS')
+    .setDescription('Documentation de l\'API avec Swagger')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+
+}
+bootstrap();
