9936c671de1369da53f5c21bdb4b52b7614c1cff
Valérie Song <valerie.song@ormex.io>
2025-02-07 14:20:16 +0100

    début test réservations

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -16,10 +16,29 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  // it('/ (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/')
+  //     .expect(200)
+  //     .expect('Hello World!');
+  // });
+
+  it('/reservations (GET) retourne la liste des réservations', async () => {
+    const response = await request(app.getHttpServer()).get('/reservations');
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it('/reservations (POST) crée une réservation', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/reservations')
+      .send({
+        "userId": 1,
+        "movieId": 1255788,
+        "startTime": "2025-09-08T20:00:00"
+      });
+    console.log('response.body', response.body);
+
+    expect(response.status).toBe(201);
   });
 });
